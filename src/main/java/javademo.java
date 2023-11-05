baa0ad80d03f5608e423ce5e1b126cae149b6cca
shaik1238 <snannukarimulla@gmail.com>
2023-11-05 13:51:21 +0530

    Initial commit

---
+++ src/main/java/javademo.java
@@ -0,0 +1,15 @@
+public class javademo {
+    int a = 4;
+    int b = 5;
+    int c =a+b;
+    void display()
+    {
+        System.out.println(c);
+    }
+    public static void main(String []args)
+    {
+        javademo j = new javademo();
+        j.display();
+    }
+
+}
